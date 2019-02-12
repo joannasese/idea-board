93ca227503601be46be1c0695ca802ace7b4a01b
Joanna Sese <jmgsese@gmail.com>
2019-02-11 18:59:51 -0600

    Add React client side, first components

---
+++ ideaboard/src/components/IdeasContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class IdeasContainer extends Component {
+  render() {
+    return (
+      <div>
+        Ideas
+      </div>
+    )
+  }
+}
